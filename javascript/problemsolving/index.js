cd212c9129ae6159a2ba5df675c9af2524b4b423
Muradazzeh <morad.azza@yahoo.com>
2022-11-08 13:33:27 +0200

    problem solving

---
+++ javascript/problemsolving/index.js
@@ -0,0 +1,41 @@
+
+/// Socumteck question
+let str1="eat",str2="ate"
+//str1="bar " str2="bare"
+//str1="bba"  str2="aab"
+
+function compare(str1, str2) {
+  let obj1 = {};
+
+  if (str1.length != str2.length) return false;
+  for (let i = 0; i < str1.length; i++) {
+    if (obj1[str1[i]]) {
+      obj1[str1[i]] += 1;
+    } else {
+      obj1[str1[i]] -= 1;
+    }
+    if (obj1[str2[i]]) {
+        obj1[[i]] += 1;
+      } else {
+        obj1[str1[i]] -= 1;
+      }
+  }
+
+}
+
+console.log(compare(str1, str2))
+
+
+/// Aspire array question
+
+
+let arr = [-1,2,3,4,-5,-6,7,-8,9,10,0,-15]
+
+for (let i= 0; i<arr.length; i++) {
+
+if(arr[i]%2!==0 && arr[i]<0 )
+
+console.log(arr[i])
+
+
+}
